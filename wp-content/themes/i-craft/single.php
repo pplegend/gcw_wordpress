e7071c901651a6aba2cb07b58dbf60b1bc54822b
zhangyongzhen <zhangyongzhen@zhangyongzhendeMacBook-Pro.local>
2015-06-07 22:52:51 +0800

    change video single page

--- wp-content/themes/i-craft/single.php
+++ wp-content/themes/i-craft/single.php
@@ -15,7 +15,6 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 			<?php $post_type = get_post_type(get_the_ID()); ?>
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-               <?php if ($post_type != "videos") : ?>
                 <div class="meta-img">
                 <?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
                     <div class="entry-thumbnail">
@@ -31,36 +30,11 @@ get_header(); ?>
                 </div>
                 
                 <div class="post-mainpart">    
-                    <header class="entry-header">
-                        <div class="entry-meta">
-                            <?php icraft_entry_meta(); ?>
-                            <?php edit_post_link( __( 'Edit', 'i-craft' ), '<span class="edit-link">', '</span>' ); ?>
-                        </div><!-- .entry-meta -->
-                    </header><!-- .entry-header -->
-                
-                    <div class="entry-content">
-                        <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'i-craft' ) ); ?>
-                        <?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'i-craft' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
-                    </div><!-- .entry-content -->
-
-                	<?php if ( is_single() && get_the_author_meta( 'description' ) && is_multi_author() ) : ?>
-                    	<footer class="entry-meta">
-                            <?php get_template_part( 'author-bio' ); ?>
-                    	</footer><!-- .entry-meta -->
-					<?php endif; ?>
-                </div>
-				<?php else: ?>
-                <div class="post-mainpart">    
                 
                     <div class="entry-content video-content">
 					   <div class="container">
 					     <div class="row">
-						   <div class="col-md-8 col-xs-12 col-sm-9 video-frame">
 							<?php echo get_the_content(); ?>
-						   </div>
-						   <div class="col-md-4 col-xs-12 col-sm-9"> 
-						    <?php echo get_the_content(); ?>
-						   </div>
 						 </div>
                         
 					   </div>
@@ -69,8 +43,7 @@ get_header(); ?>
                     </div><!-- .entry-content -->
 
                 </div>				  
-				
-				<?php endif; ?>
+		
             </article><!-- #post -->    
     
 
